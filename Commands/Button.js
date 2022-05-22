8b5a629ff56131eb2b37610a7228db486eb15fb2
Emrefxm <104074906+Emrefxm@users.noreply.github.com>
2022-05-22 10:38:09 +0300

    Create Button.js

---
+++ Commands/Button.js
@@ -0,0 +1,40 @@
+
+const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js")
+
+    const embed = new MessageEmbed()
+
+    .setFooter(Lunes Was Here!);
+
+    let lunes = new MessageButton()
+
+    .setStyle("PRIMARY")
+
+    .setLabel("Lunes")
+
+    .setCustomId("l1")
+
+    let lunes2 = new MessageButton()
+
+    .setStyle("PRIMARY")
+
+    .setLabel("Lunes")
+
+    .setCustomId("l2")
+
+    let lunes3 = new MessageButton()
+
+    .setStyle("PRIMARY")
+
+    .setLabel("Lunes")
+
+    .setCustomId("l3")
+
+    const row = new MessageActionRow() 
+
+    .addComponents([lunes1, lunes2,lunes3]);
+
+    message.channel.send({components: [row], embeds: [embed] });
+
+  }
+
+}
